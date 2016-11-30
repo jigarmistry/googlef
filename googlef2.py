1b1f46fad8d110013be2fbbaefd1d82a31b330af
Jigar Mistry <jigarmistry24@gmail.com>
2016-11-30 07:15:27 +0530

    New function for day change

--- googlef2.py
+++ googlef2.py
@@ -40,6 +40,16 @@ def get_finance_data():
     return header_data, nav_filter_list, pos_filter_list
 
 
+def calculate_day_range(last, high, low):
+    if float(last) >= 0.995*float(high):
+        return "HIGH",1
+    else:
+        if float(last) <= 1.005*float(low):
+            return "LOW",2
+        else:
+            return "",3    
+
+
 def generate_report(header_data, nav_list, pos_list):
     """['Name', 'Symbol', 'Last price', 'Change', 'Mkt cap', 'Volume', 'Open', 'High', 'Low', "Day's gain"]"""
 
@@ -48,21 +58,7 @@ def generate_report(header_data, nav_list, pos_list):
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <style>td {height:10px; width:150px;text-align: center;}
-        th{text-align: center;}
-tbody { height: 100px;width:100%;} </style>
-    </head><body>"""
-    strNavTableHtml = """<table border="1"><caption class="text-center">Nagative Values</caption><thead><tr>
-    <th>Symbol</th><th>Last Price</th><th>Change</th><th>Open</th>
-    <th>% Net Change</th><th>High</th><th>Low</th><th>Day's Range</th></tr></thead>"""
-    for nrow in nav_list:
-        strNrow = """<tr>"""
-        strNrow = strNrow + "<td style='color:#1893f2'>"+nrow[1]+"</td><td>"+nrow[2]+"</td><td style='color:#ed5353'>"+nrow[3]+"</td><td>"+nrow[6]+"</td>"
-        strNrow = strNrow + "<td>"+format((float(nrow[2])-float(nrow[6]))/float(nrow[6]), '.4f')+"%</td>"
-        strNrow = strNrow + "<td>"+nrow[7]+"</td><td>"+nrow[8]+"</td><td></td>"
-        strNrow = strNrow + "</tr>"
-        strNavTableHtml = strNavTableHtml + strNrow
-    strNavTableHtml = strNavTableHtml + "</tbody></table>"
-    strHtml = strHtml + strNavTableHtml
+        th{text-align: center;} tbody { height: 100px;width:100%;} </style> </head><body>"""
 
     strPosTableHtml = """<table border="1"><caption class="text-center">Positive Values</caption><thead><tr>
     <th>Symbol</th><th>Last Price</th><th>Change</th><th>Open</th>
@@ -70,12 +66,39 @@ tbody { height: 100px;width:100%;} </style>
     for prow in pos_list:
         strProw = """<tr>"""
         strProw = strProw + "<td style='color:#1893f2'>"+prow[1]+"</td><td>"+prow[2]+"</td><td style='color:#3fc151'>"+prow[3]+"</td><td>"+prow[6]+"</td>"
-        strProw = strProw + "<td>"+format((float(prow[2])-float(prow[6]))/float(prow[6]), '.4f')+"%</td>"
-        strProw = strProw + "<td>"+prow[7]+"</td><td>"+prow[8]+"</td><td></td>"
+        strProw = strProw + "<td>"+format((float(prow[2])-float(prow[6]))/float(prow[6]), '.4f')+"%</td>"        
+        strProw = strProw + "<td>"+prow[7]+"</td><td>"+prow[8]+"</td>"
+        value,st = calculate_day_range(prow[2], prow[7], prow[8])
+        if st == 1:
+            strProw = strProw + "<td style='background-color:#3fc151;color:green'>"+value+"</td>"
+        elif st == 2:
+            strProw = strProw + "<td style='background-color:#ed5353;color:red'>"+value+"</td>"
+        else:
+            strProw = strProw + "<td></td>"              
         strProw = strProw + "</tr>"
         strPosTableHtml = strPosTableHtml + strProw
     strPosTableHtml = strPosTableHtml + "</tbody></table>"
     strHtml = strHtml + strPosTableHtml
+
+    strNavTableHtml = """<table border="1"><caption class="text-center">Nagative Values</caption><thead><tr>
+    <th>Symbol</th><th>Last Price</th><th>Change</th><th>Open</th>
+    <th>% Net Change</th><th>High</th><th>Low</th><th>Day's Range</th></tr></thead>"""
+    for nrow in nav_list:
+        strNrow = """<tr>"""
+        strNrow = strNrow + "<td style='color:#1893f2'>"+nrow[1]+"</td><td>"+nrow[2]+"</td><td style='color:#ed5353'>"+nrow[3]+"</td><td>"+nrow[6]+"</td>"
+        strNrow = strNrow + "<td>"+format((float(nrow[2])-float(nrow[6]))/float(nrow[6]), '.4f')+"%</td>"
+        strNrow = strNrow + "<td>"+nrow[7]+"</td><td>"+nrow[8]+"</td>"
+        value,st = calculate_day_range(nrow[2], nrow[7], nrow[8])
+        if st == 1:
+            strNrow = strNrow + "<td style='background-color:#3fc151;color:green'>"+value+"</td>"
+        elif st == 2:
+            strNrow = strNrow + "<td style='background-color:#ed5353;color:red'>"+value+"</td>"
+        else:
+            strNrow = strNrow + "<td></td>"
+        strNrow = strNrow + "</tr>"
+        strNavTableHtml = strNavTableHtml + strNrow
+    strNavTableHtml = strNavTableHtml + "</tbody></table>"
+    strHtml = strHtml + strNavTableHtml
 
     strHtml = strHtml + "</body></html>"
     html_file = open("report.html","w")
